c3479d8c64a3713a753ceb588f1c56b58aac6883
Joonas <jheiskar24@students.oamk.fi>
2025-01-20 18:28:40 +0200

    tehtava palautus

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+int arvaukset=0;
+int maxnum;
+
+int game(int maxnum)
+{
+    srand(11);
+    cout <<"anna maksimiarvo etsittavalle luvulle" << endl;
+    cin >> maxnum;
+    int satunnaisluku = rand() % (maxnum+1);
+    bool peli=true;
+
+
+    while (peli==true){
+        cout << "yrita arvata kokonaisluku" << endl;
+
+        int arvaus;
+        cin >> arvaus;
+
+        if (arvaus < satunnaisluku){
+            arvaukset++;
+            cout << "arvauksesi on pienempi kuin etsittava luku" << endl;
+        }
+        else if (arvaus > satunnaisluku) {
+            arvaukset++;
+            cout << "arvauksesi on suurempi kuin etsittava luku"<< endl;
+        }
+        else{
+            arvaukset++;
+            cout << "arvauksesi on yhtasuuri kuin etsittava luku. Voitit!"<< endl;
+            peli=false;
+        }
+    }
+    return arvaukset;
+
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  game(maxnum);
+    cout << "arvausten maara " << arvaukset << endl;
 }
